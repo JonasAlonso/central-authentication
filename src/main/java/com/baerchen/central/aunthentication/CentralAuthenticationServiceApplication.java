96da5f33396890775dadf05a23d9231ff066654e
JonasAlonso <jonasalonsolanza@gmail.com>
2025-06-28 15:42:38 +0200

    CAUTH-101 first commit

---
+++ src/main/java/com/baerchen/central/aunthentication/CentralAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.baerchen.central.aunthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralAuthenticationServiceApplication.class, args);
+	}
+
+}
