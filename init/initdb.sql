ebe140721934060ed1900ce77f5d3285bb2a5fe7
JonasAlonso <jonasalonsolanza@gmail.com>
2025-08-05 00:07:57 +0200

    CAUTH-202 new initdb.sql from the template project

--- init/initdb.sql
+++ init/initdb.sql
@@ -1,15 +1,28 @@
+-- Drop existing schema and user
 DROP SCHEMA IF EXISTS ${ENV_PREFIX}_${database.schema} CASCADE;
 
 DROP ROLE IF EXISTS ${ENV_PREFIX}_${database.user};
 
+-- Create new user
 CREATE ROLE ${ENV_PREFIX}_${database.user} LOGIN PASSWORD '${database.pswd}';
 
+-- Create schema owned by the new user
 CREATE SCHEMA ${ENV_PREFIX}_${database.schema} AUTHORIZATION ${ENV_PREFIX}_${database.user};
 
+-- Tighten public access
 REVOKE ALL ON SCHEMA ${ENV_PREFIX}_${database.schema} FROM PUBLIC;
 
+-- Grant access to the new user (redundant since they are the owner, but explicit is good)
 GRANT USAGE ON SCHEMA ${ENV_PREFIX}_${database.schema} TO ${ENV_PREFIX}_${database.user};
 GRANT CREATE ON SCHEMA ${ENV_PREFIX}_${database.schema} TO ${ENV_PREFIX}_${database.user};
 
+-- 🔥 Now grant access to postgres as well
+GRANT USAGE ON SCHEMA ${ENV_PREFIX}_${database.schema} TO postgres;
+GRANT CREATE ON SCHEMA ${ENV_PREFIX}_${database.schema} TO postgres;
+
+-- Set default privileges inside this schema for both the user and postgres
 ALTER DEFAULT PRIVILEGES IN SCHEMA ${ENV_PREFIX}_${database.schema}
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO ${ENV_PREFIX}_${database.user};
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA ${ENV_PREFIX}_${database.schema}
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO postgres;
